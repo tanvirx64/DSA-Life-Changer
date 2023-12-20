cad734a81847bd13998aed175fe5c81b1f7dc4cd
Tanvir Ahmed <tanvircsdev@gmail.com>
2023-12-21 00:29:49 +0600

    pattern-2

--- C++/Pattern/pattern-2.cpp
+++ C++/Pattern/pattern-2.cpp
@@ -2,6 +2,28 @@
 #include<cstdio>
 using namespace std;
 int main(){
-    cout<<"Hello World";
-    return 0;
+// 11111
+//  2222
+//   333
+//    44
+//     5
+
+    int n, number=1;
+    cin>>n;
+    int row = 1;
+    while(row<=n){
+        int space = row - 1;
+        while (space)
+        {
+            cout<<" ";
+            space--;
+        }
+        int star = n - row + 1;
+        while( star ){
+            cout<<row;
+            star--;
+        }
+        cout<<endl;
+        row++;
+    }
 }
