5de361d015ec80a2ab2c388bf952014929efb55e
Tanvir Ahmed <tanvircsdev@gmail.com>
2023-12-21 00:23:55 +0600

    pattern-1

--- C++/Pattern/pattern-1.cpp
+++ C++/Pattern/pattern-1.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
-#include<cstdio>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    cout<<"Hello World";
+int main()
+{
+
+//     1
+//    22
+//   333
+//  4444
+// 55555
+
+    int n, number=1;
+    cin>>n;
+    
+    int row = 1;
+    while(row<=n){
+        int space = n-row;
+        while(space){
+            cout<<" ";
+            space--;
+        }
+
+        int number = row;
+        while(number){
+            cout<<row;
+            number--;
+        }
+        cout<<endl;
+
+        row++;
+    }
     return 0;
 }
